c69b228e7a8dc14f2b7e87fcdf69ddcfdff7a579
Amir Barylko <amir@barylko.com>
2017-04-15 19:31:58 -0700

    Added rule in site.hs to include favicon*.*

--- site.hs
+++ site.hs
@@ -21,12 +21,16 @@ main = hakyll $ do
       route   idRoute
       compile compressCssCompiler
 
+    match "favicon*.*" $ do
+      route   idRoute
+      compile copyFileCompiler
+
     match (fromList ["about.html"]) $ do
-        route   $ setExtension "html"
-        compile $ 
-          pandocCompiler
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
-          >>= relativizeUrls
+      route   $ setExtension "html"
+      compile $ 
+        pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
@@ -53,18 +57,18 @@ main = hakyll $ do
 
 
     match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let indexCtx =
+                listField "posts" postCtx (return posts) `mappend`
+                constField "title" "Home"                `mappend`
+                defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+        getResourceBody
+            >>= applyAsTemplate indexCtx
+            >>= loadAndApplyTemplate "templates/default.html" indexCtx
+            >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
